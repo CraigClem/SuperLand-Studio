71f2cd91c5cf29b304bc6e5758f8136fe797ca76
Craig <craigclement@hotmail.com>
2021-12-10 09:42:41 +0000

    changes to font sizes and contact form

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -56,24 +56,31 @@ function Home() {
           </div>
         </section>
 
-        <section className="bg-gray-400 w-full h-96 my-2 flex align-middle">
+        <section className="bg-gray-400 w-full h-screen my-2 flex align-middle">
           <div className="ml-14">
-            <p className="text-white font-extrabold text-xl md:text-2xl lg:text-3xl tracking-wide py-1 lg:leading-10">REGNERATIVE DESIGN</p>
-            <p className="text-gray-600 font-extrabold text-xl md:text-2xl lg:text-3xl py-1 tracking-wide lg:leading-10">FOR BUILDINGS,</p>
-            <p className="text-gray-600 font-extrabold text-xl md:text-2xl lg:text-3xl py-1 tracking-wide lg:leading-10">FOR LANDSCAPES,</p>
-            <p className="text-gray-600 font-extrabold text-xl md:text-2xl lg:text-3xl py-1 tracking-wide lg:leading-10">FOR COMMUNITIES.</p>
+            <p className="text-white font-extrabold text-6xl tracking-wide py-1 lg:leading-10 mb-8">REGNERATIVE DESIGN</p>
+            <br />
+            <p className="text-gray-600 font-extrabold text-6xl py-1 tracking-wide lg:leading-10 my-8">FOR BUILDINGS,</p>
+            <br />
+            <p className="text-gray-600 font-extrabold text-6xl py-1 tracking-wide lg:leading-10 my-8">FOR LANDSCAPES,</p>
+            <br />
+            <p className="text-gray-600 font-extrabold text-6xl py-1 tracking-wide lg:leading-10 my-8">FOR COMMUNITIES.</p>
           </div>
         </section>
 
         <section className="grid grid-cols-2 w-full">
 
-          {/* <div className="soil-hands w-full h-full mx-4 mt-2 mb-4"> */}
-          {/* <Image cloudName="drevpwd0d" publicId="hands_in_soil_FINAL_d9lgrq">
+          <div className="w-full h-full mx-4 mt-2 mb-4">
+            {/* <Image cloudName="drevpwd0d" publicId="hands_in_soil_FINAL_d9lgrq">
               <Transformation height="250" width="300" crop="fill" quality="100" />
             </Image> */}
-          {/* </div> */}
+            {/* </div> */}
+            <Image cloudName="drevpwd0d" publicId="Sarah_photo_b_w_w5xzx4.jpg" >
 
-          <div className="mx-4 my-2 tracking-wide text-xs lg:text-xl">
+            </Image>
+          </div>
+
+          <div className="mx-12 my-2 px-8 tracking-wide text-xs md:text-xl lg:text-2xl">
             <p className="flex pb-2">We specialise in residential and conservation projects, as well as community focused builds. </p>
             <p className="font-bold pb-2">Our designs help people transition to a more
               sustainable, carbon-free future. We focus on:</p>
@@ -89,7 +96,6 @@ function Home() {
           </div>
 
         </section>
-
       </main>
       <Contact />
     </>
